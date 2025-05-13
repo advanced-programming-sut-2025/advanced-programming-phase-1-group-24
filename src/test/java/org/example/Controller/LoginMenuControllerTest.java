6265273b14a4e2e190f889d46d8ea52d4ebf2362
ZahraAmirbeigy <zahraamirbeigy24@gmail.com>
2025-05-13 16:44:13 +0330

    starting tests

---
+++ src/test/java/org/example/Controller/LoginMenuControllerTest.java
@@ -0,0 +1,40 @@
+package org.example.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LoginMenuControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void pickQuestion() {
+    }
+
+    @Test
+    void forgetPassword() {
+    }
+
+    @Test
+    void isStrongPassword() {
+    }
+
+    @Test
+    void isValidEmail() {
+    }
+
+    @Test
+    void isValidUsername() {
+    }
+}
