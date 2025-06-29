107083d867e5a0aa4da8209b4e9947da066c8c82
ZahraAmirbeigy <zahraamirbeigy24@gmail.com>
2025-06-29 19:47:47 +0330

    salam phase2

---
+++ core/src/main/java/io/github/stardew/mini/model/item/TileDescriptionId.java
@@ -0,0 +1,20 @@
+package io.github.stardew.mini.model.item;
+
+// TileDescriptionId.java
+public enum TileDescriptionId {
+    SOIL("game/tiles/soil.png"),
+    GRASS("game/tiles/grass.png"),
+    WATER("game/tiles/water.png"),
+    SLOT("game/tiles/slot.png"),
+    HIGHLIGHT("game/tiles/highlight.png"),
+    ;
+    final String iconPath;
+
+    TileDescriptionId(String iconPath) {
+        this.iconPath = iconPath;
+    }
+
+    public String getIconPath() {
+        return iconPath;
+    }
+}
