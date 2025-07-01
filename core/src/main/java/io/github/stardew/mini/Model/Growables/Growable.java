a36e7a15ba5a3ec6bc3bf602ec78ad8b9378634c
kimia <kim05hey@gmail.com>
2025-07-01 12:01:28 +0330

    commite akhare nikki

---
+++ core/src/main/java/io/github/stardew/mini/Model/Growables/Growable.java
@@ -0,0 +1,192 @@
+package io.github.stardew.mini.Model.Growables;
+
+import io.github.stardew.mini.Model.Things.Item;
+import io.github.stardew.mini.Model.Things.ProductQuality;
+import io.github.stardew.mini.Model.TimeManagement.Season;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Growable extends Item { //extend Item
+    //note that we cannot put tree or crops in inventory  only seeds and products
+    //When the growable is added to a tile we will fill out the containedGrowable field in the tile
+    SourceType source;
+    GrowableType growableType;
+    int age;
+    int currentStage; //It is 1 if it has been planted
+    boolean isWateredToday;
+    boolean hasBeenFertalized;
+    boolean hasBeenAttackedByCrow;
+    int daysLeftToDie = 2;
+    //two boolean is null one is full
+    TreeType treeType;
+    CropType cropType;
+    ForagingCropType foragingCropType;
+
+    //when the crop/tree is ready to harvest if we can only harvest it once, we will change the growableType ,
+    //else we will create a copy of this growable and put the growableType as product/plant and we make the age and stage of the initial growable 0, we will add the product to the tile
+    //if the product of growable(tree) is coal then create an Item that its type is coal
+    //note that if growableType is fruit we can find the fruitType from the filled treeType
+    public Growable() {
+        super("Unknown", true, 0, false, ProductQuality.Normal, false); // default values
+    }
+
+    //if we ever add an ArrayList or List to this class we need to do a deep copy
+    public Growable copy() {
+        Growable g = new Growable();
+        g.source = this.source;
+        g.growableType = this.growableType;
+        g.age = this.age;
+        g.currentStage = this.currentStage;
+        g.isWateredToday = this.isWateredToday;
+        g.hasBeenFertalized = this.hasBeenFertalized;
+        g.hasBeenAttackedByCrow = this.hasBeenAttackedByCrow;
+        g.daysLeftToDie = this.daysLeftToDie;
+        g.treeType = this.treeType;
+        g.cropType = this.cropType;
+        g.foragingCropType = this.foragingCropType;
+        g.setName(this.getName());
+        g.setPrice(this.getPrice());
+        g.setSellable(this.isSellable());
+        g.setQuality(this.getProductQuality());
+        g.setPlaceable(this.isPlaceable());
+        if (g.getCropType() != null) {
+            g.setEatable(g.getCropType().getIsEdible());
+        } else {
+            g.setEatable(false);
+        }
+
+
+        return g;
+    }
+
+//    //if we ever add an ArrayList or List to this class we need to do a deep copy
+//    @Override
+//    public Growable clone() {
+//        try {
+//            return (Growable) super.clone(); // Shallow copy (OK if fields are primitives or enums)
+//        } catch (CloneNotSupportedException e) {
+//            throw new AssertionError(); // Should never happen
+//        }
+//    }
+
+    public SourceType getSource() {
+        return source;
+    }
+
+    public GrowableType getGrowableType() {
+        return growableType;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCurrentStage() {
+        return currentStage;
+    }
+
+    public TreeType getTreeType() {
+        return treeType;
+    }
+
+    public CropType getCropType() {
+        return cropType;
+    }
+
+    public ForagingCropType getForagingCropType() {
+        return foragingCropType;
+    }
+
+    public int getDaysLeftToDie() {
+        return daysLeftToDie;
+    }
+
+    public boolean getIsWateredToday() {
+        return isWateredToday;
+    }
+
+    public boolean hasBeenFertalized() {
+        return hasBeenFertalized;
+    }
+
+    public boolean hasBeenAttackedByCrow() {
+        return hasBeenAttackedByCrow;
+    }
+
+    public void grow() {
+    }
+
+    private static final Map<Season, List<SourceType>> mixedSeedsMap = Map.of(
+            Season.SPRING, List.of(
+                    SourceType.CauliflowerSeeds,
+                    SourceType.ParsnipSeeds,
+                    SourceType.PotatoSeeds,
+                    SourceType.JazzSeeds,
+                    SourceType.TulipBulb
+            ),
+            Season.SUMMER, List.of(
+                    SourceType.CornSeeds,
+                    SourceType.PepperSeeds,
+                    SourceType.RadishSeeds,
+                    SourceType.WheatSeeds,
+                    SourceType.PoppySeeds,
+                    SourceType.SunflowerSeeds,
+                    SourceType.SummerSquashSeeds
+            ),
+            Season.AUTUMN, List.of(
+                    SourceType.ArtichokeSeeds,
+                    SourceType.CornSeeds,
+                    SourceType.EggplantSeeds,
+                    SourceType.PumpkinSeeds,
+                    SourceType.SunflowerSeeds,
+                    SourceType.FairySeeds
+            ),
+            Season.WINTER, List.of(
+                    SourceType.PowdermelonSeeds
+            )
+    );
+
+    public static List<SourceType> getMixedSeeds(Season season) {
+        return mixedSeedsMap.getOrDefault(season, List.of());
+    }
+
+
+    public static SourceType getRandomSourceType(Season season) {
+        List<SourceType> sourceTypes = mixedSeedsMap.get(season);
+        if (sourceTypes == null || sourceTypes.isEmpty()) {
+            throw new IllegalArgumentException("No source types for season: " + season);
+        }
+        int index = ThreadLocalRandom.current().nextInt(sourceTypes.size());
+        return sourceTypes.get(index);
+    }
+
+    public void setGrowableType(GrowableType growableType) {
+        this.growableType = growableType;
+    }
+
+    public void setCurrentStage(int currentStage) {
+        this.currentStage = currentStage;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDaysLeftToDie(int daysLeftToDie) {
+        this.daysLeftToDie = daysLeftToDie;
+    }
+
+    public void setIsWateredToday(boolean wateredToday) {
+        isWateredToday = wateredToday;
+    }
+
+    public void setHasBeenFertalized(boolean hasBeenFertalized) {
+        this.hasBeenFertalized = hasBeenFertalized;
+    }
+
+    public void setHasBeenAttackedByCrow(boolean hasBeenAttackedByCrow) {
+        this.hasBeenAttackedByCrow = hasBeenAttackedByCrow;
+    }
+}
---
+++ core/src/main/java/io/github/stardew/mini/Model/Things/Fish.java
@@ -0,0 +1,35 @@
+package io.github.stardew.mini.Model.Things;
+
+import java.util.Objects;
+
+public class Fish extends Item {
+    private FishType type;
+
+    public Fish(ProductQuality productQuality, FishType type) {
+        super(type.getName(), true, type.getSellPrice(), false, productQuality,false);
+        this.type = type;
+    }
+
+
+    public FishType getType() {
+        return type;
+    }
+    @Override
+    public Fish copy() {
+        return new Fish(productQuality, type);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fish that)) return false;
+        if (!super.equals(o)) return false;
+        return type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), type);
+    }
+
+}
+
