30bc57b4d81daec52e6c946866f9e8f77e7a30d4
ZahraAmirbeigy <zahraamirbeigy24@gmail.com>
2025-07-07 14:25:02 +0330

    petting animals

--- core/src/main/java/io/github/stardew/mini/View/HeartEffect.java
+++ core/src/main/java/io/github/stardew/mini/View/HeartEffect.java
@@ -3,6 +3,7 @@ package io.github.stardew.mini.View;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.github.stardew.mini.Model.Assets.GameAssetManager;
 
 
 public class HeartEffect {
@@ -25,10 +26,22 @@ public class HeartEffect {
         y += 30 * delta;
     }
 
-    public void draw(SpriteBatch batch) {
-        float alpha = 1f - (time / duration);
-        Color old = batch.getColor();
-        batch.setColor(1, 1, 1, alpha);
-        batch.setColor(old);
+//    public void draw(SpriteBatch batch) {
+//        float alpha = 1f - (time / duration);
+//        Color old = batch.getColor();
+//        batch.setColor(1, 1, 1, alpha);
+//        batch.draw(GameAssetManager.SECRET_HEART, x, y, 32, 32);
+//        batch.setColor(old);
+//    }
+public void draw(SpriteBatch batch) {
+    float scale = 1f + (0.5f * (1f - time / duration)); // grow slightly
+    float size = 32 * scale;
+    batch.setColor(1, 1, 1, 1f - (time / duration));
+    batch.draw(GameAssetManager.SECRET_HEART, x, y, size, size);
+    batch.setColor(Color.WHITE);
+}
+
+    public boolean isFinished() {
+        return finished;
     }
 }
